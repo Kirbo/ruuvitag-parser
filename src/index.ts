77521817326d26668d294a8a56de0a304c714fd2
Kimmo Saari <kimmo.saari@aava.fi>
2021-09-09 15:05:39 +0300

    fix: exports

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,20 @@ import {
 } from '@lib/parser'
 import { parseEddystone } from '@lib/eddystone'
 
+export {
+  parseUrl,
+  parseData,
+  parseEddystone,
+  formats_2_and_4,
+  format_3,
+  format_5,
+}
+
 export default {
   parseUrl,
   parseData,
   parseEddystone,
-
   formats_2_and_4,
-
-  format_2: formats_2_and_4,
-  format_4: formats_2_and_4,
   format_3,
   format_5,
 }
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -8,9 +8,11 @@ import {
 export { ParsedFormatV2, ParsedFormatV3, ParsedFormatV4, ParsedFormatV5 }
 
 declare module 'ruuvitag-parser' {
-  function parseUrl(url: string): ParsedFormatV2 | ParsedFormatV4 | Error
-  function parseData(data: Buffer): ParsedFormatV3 | ParsedFormatV5 | Error
-  function parseEddystone(data: Buffer): string | void
+  export function parseUrl(url: string): ParsedFormatV2 | ParsedFormatV4 | Error
+  export function parseData(
+    data: Buffer,
+  ): ParsedFormatV3 | ParsedFormatV5 | Error
+  export function parseEddystone(data: Buffer): string | void
 
   export namespace formats_2_and_4 {
     function parse(data: Buffer): ParsedFormatV2 | ParsedFormatV4
